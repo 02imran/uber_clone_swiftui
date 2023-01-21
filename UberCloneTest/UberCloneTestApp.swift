6caacf5c94249f6c54b264abda2ece231a21bec6
imran <imran40031@gmail.com>
2023-01-21 19:27:40 +0600

    Initial Commit

---
+++ UberCloneTest/UberCloneTestApp.swift
@@ -0,0 +1,17 @@
+//
+//  UberCloneTestApp.swift
+//  UberCloneTest
+//
+//  Created by md imran on 21/1/23.
+//
+
+import SwiftUI
+
+@main
+struct UberCloneTestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
